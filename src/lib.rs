837f5071f50cb958007584a87dea71cd875329fa
p0lunin <dmytro.polunin@gmail.com>
2020-12-26 00:34:19 +0200

    sealing the structs, make Types[Not]Eq public, renaming

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,12 @@
 //!
 //! You can use a trait for checking of equality instead:
 //! ```rust
-//! use compile_type_eq::CompileEq;
+//! use compile_type_eq::*;
 //!
 //! fn do_some_when_not_eq<T, U, BecauseTypesMustBeNotEq>(t: T, u: U) -> String
 //! where
-//!     T: CompileEq<U, BecauseTypesMustBeNotEq>
+//!     T: CompileEq<U, BecauseTypesMustBeNotEq>,
+//!     BecauseTypesMustBeNotEq: CompileEqResult
 //! {
 //!     "Some".to_string()
 //! }
@@ -31,11 +32,12 @@
 //! ```
 //!
 //! ```compile_fail
-//! use compile_type_eq::CompileEq;
+//! use compile_type_eq::*;
 //!
 //! fn do_some_when_not_eq<T, U, BecauseTypesMustBeNotEq>(t: T, u: U) -> String
 //! where
-//!     T: CompileEq<U, BecauseTypesMustBeNotEq>
+//!     T: CompileEq<U, BecauseTypesMustBeNotEq>,
+//!     BecauseTypesMustBeNotEq: CompileEqResult
 //! {
 //!     "Some".to_string()
 //! }
@@ -43,15 +45,17 @@
 //! do_some_when_not_eq("a", "b");
 //! ```
 
-use crate::structs::{Eq, NotEq};
+pub use structs::{TypesEq, CompileEqResult};
 
 mod structs;
 
-/// Trait for compile-time equality checking
-pub trait CompileEq<U, Inf> {}
+use structs::TypesNotEq;
 
-impl<T> CompileEq<T, Eq> for T {}
-impl<T, U> CompileEq<U, NotEq> for T {}
+/// Trait for compile-time equality checking
+pub trait CompileEq<U, Result: CompileEqResult> {}
+
+impl<T> CompileEq<T, TypesEq> for T {}
+impl<T, U> CompileEq<U, TypesNotEq> for T {}
 
 /// Checks that the types are eq.
 ///
@@ -69,7 +73,7 @@ impl<T, U> CompileEq<U, NotEq> for T {}
 /// ```
 pub fn assert_types_eq<T, U>()
 where
-    T: CompileEq<U, Eq>,
+    T: CompileEq<U, TypesEq>,
 {
 }
 
@@ -90,5 +94,6 @@ where
 pub fn assert_types_not_eq<T, U, BecauseTypesMustBeNotEq>()
 where
     T: CompileEq<U, BecauseTypesMustBeNotEq>,
+    BecauseTypesMustBeNotEq: CompileEqResult
 {
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,2 +1,24 @@
-pub struct Eq;
-pub struct NotEq;
+/// Use this struct when you want to check that two types are equal.
+///
+/// Examples:
+/// ```rust
+/// use compile_type_eq::*;
+///
+/// fn foo<T, U>() where T: CompileEq<U, TypesEq> {}
+/// ```
+pub struct TypesEq;
+pub struct TypesNotEq;
+
+pub struct Sealed;
+
+/// Trait is used to be affirmative that there are no external implementations for `CompileEq`.
+pub trait CompileEqResult {
+    fn assert_no_external_implements(_: Sealed);
+}
+
+impl CompileEqResult for TypesEq {
+    fn assert_no_external_implements(_: Sealed) {}
+}
+impl CompileEqResult for TypesNotEq {
+    fn assert_no_external_implements(_: Sealed) {}
+}
